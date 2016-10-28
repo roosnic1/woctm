48d98d7f39f69943a189006271340a4b1360dcf1
Immanuel Häussermann <hai@panter.ch>
2016-10-29 01:49:19 +0200

    yolo

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
 import cowsay from 'cowsay-browser'
-
+import List from '../components/list'
 
 export default () => (
   <div>
     <h1>About Us</h1>
     <pre>{ cowsay.say({ text: 'Hello My Name is.' }) }</pre>
+    <List>
+      <div>Hans</div>
+      <div>Frunzli</div>
+    </List>
   </div>
 )
