24c04678492e3ca50758f88780d036d1bcd398fa
Nicolas Roos <nicolas.k.roos@gmail.com>
2016-10-29 00:12:46 +0200

    Added additional page. YIPPIKEYE MOTHERFUCKER

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
+
+
 export default () => (
   <div>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
     <h1>Hi. I'm mobile-ready!</h1>
+    <p>Click here to learn more <Link href="/about">About Us</Link></p>
   </div>
 )
