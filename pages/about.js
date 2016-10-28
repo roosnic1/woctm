7846f89fba6f76f5621d173d365351a06c632184
Nicolas Roos <nicolas.k.roos@gmail.com>
2016-10-29 00:35:08 +0200

    added a COW that can speak

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
 import Link from 'next/link'
+import cowsay from 'cowsay-browser'
 
 
 export default () => (
   <div>
     <h1>About Us</h1>
+    <pre>{ cowsay.say({ text: 'Hello My Name is.' }) }</pre>
   </div>
 )
