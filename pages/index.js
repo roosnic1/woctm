da7200d43ff0effc3dcc22ca5cde131c17584896
Nicolas Roos <nicolas.k.roos@gmail.com>
2016-10-29 03:55:37 +0200

    add links

--- pages/index.js
+++ pages/index.js
@@ -8,19 +8,26 @@ import NextButton from '../components/nextButton';
 import Wrapper from '../components/wrapper';
 import Header from '../components/header';
 
-export default () => {
-	const title = `Soupify / Souperb / Soupé`;
+export default class extends React.Component {
 
-	return (
-	  <div>
-      <Header />
-      <Wrapper>
-	      <h1>{title}</h1>
-        <TextInput></TextInput>
-	      <p>Click here to learn more <Link href="/about">About Us</Link></p>
-	      <p>Fail <Link href="/asdas">here</Link></p>
-        <NextButton linkText="Next" link="page1"></NextButton>
-      </Wrapper>
-	  </div>
-	);
+  constructor (props) {
+    super(props)
+  }
+
+	render () {
+    const title = `Soupify / Souperb / Soupé`;
+
+    return (
+  	  <div>
+        <Header />
+        <Wrapper>
+  	      <h1>{title}</h1>
+          <TextInput></TextInput>
+  	      <p>Click here to learn more <Link href="/about">About Us</Link></p>
+  	      <p>Fail <Link href="/asdas">here</Link></p>
+          <NextButton linkText="Next" link="page1" url={this.props.url}></NextButton>
+        </Wrapper>
+  	  </div>
+  	);
+  }
 }
